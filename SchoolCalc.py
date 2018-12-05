041059f4d0afce2eedae8a347eba5423b97a6afa
Rob <Zhouenkissinger@gmail.com>
2018-12-05 13:40:28 -0500

    Starting work

---
+++ SchoolCalc.py
@@ -0,0 +1,16 @@
+import csv, os
+
+outputFile = open('schoollistoutput.csv', 'w', newline = '')
+outputWrite = csv.writer(outputFile)
+
+schoolList = []
+
+
+#open CSV
+
+#for loop every row
+
+# remove last three char from every school
+
+# check to see if that string is in a list, if not append. Else ignore.
+
